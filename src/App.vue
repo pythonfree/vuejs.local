fe1b46b480d809ad4776c50735abfcad6af83a7f
shahov.e <shahov.e@oneway.su>
2022-02-26 01:57:24 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Hello Medium clone</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/Register.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sing Up</h1>
+          <p class="text-xs-center">
+            <router-link to="{name: 'login'}">
+              Need an account?
+            </router-link>
+          </p>
+            VALIDATION ERRORS
+            <form @submit.prevent="onSubmit">
+
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvRegister'
+}
+</script>
