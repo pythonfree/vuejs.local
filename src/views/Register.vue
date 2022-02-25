95cebbfeb067e4216ab93fb704de972fb55148d3
shahov.e <shahov.e@oneway.su>
2022-02-26 02:24:09 +0300

    6

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,14 +5,23 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sing Up</h1>
           <p class="text-xs-center">
-            <router-link to="{name: 'login'}">
+            <router-link :to="{name: 'login'}">
               Need an account?
             </router-link>
           </p>
-            VALIDATION ERRORS
-            <form @submit.prevent="onSubmit">
-
-            </form>
+          VALIDATION ERRORS
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input type="text" class="form-control form-control-lg" placeholder="Username">
+            </fieldset>
+            <fieldset class="form-group">
+              <input type="text" class="form-control form-control-lg" placeholder="Email">
+            </fieldset>
+            <fieldset class="form-group">
+              <input type="password" class="form-control form-control-lg" placeholder="Password">
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">Sign Up</button>
+          </form>
         </div>
       </div>
     </div>
@@ -21,6 +30,11 @@
 
 <script>
 export default {
-  name: 'McvRegister'
+  name: 'McvRegister',
+  methods: {
+    onSubmit() {
+      console.log('subbmitted form')
+    }
+  }
 }
 </script>
