815193f24fa94dba432c56dab88b2fa2811d90ad
shahov.e <shahov.e@oneway.su>
2022-02-26 10:13:54 +0300

    Кнопка Sign Up

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -20,10 +20,10 @@
             <fieldset class="form-group">
               <input type="password" class="form-control form-control-lg" placeholder="Password">
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign Up</button>
+            <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmitting">
+              Sign Up
+            </button>
           </form>
-          <button @click="increaseCounter">Increase counter</button>
-          {{count}}
         </div>
       </div>
     </div>
@@ -34,18 +34,15 @@
 export default {
   name: 'McvRegister',
   computed: {
-    count() {
-      return this.$store.state.count
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
     }
   },
   methods: {
     onSubmit() {
       console.log('subbmitted form')
+      this.$store.commit('registerStart')
     },
-    increaseCounter() {
-      console.log('increaseCounter')
-      this.$store.commit('increment')
-    }
   }
 }
 </script>
